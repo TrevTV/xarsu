7d199de32934100edab4ee3361877fc4347e4f5e
Trevor <me@trev.app>
2026-07-27 19:58:53 -0400

    Add logger system to Reference

---
+++ reference/Logger.cs
@@ -0,0 +1,11 @@
+﻿namespace xarsu.Reference;
+
+public class Logger(string tag)
+{
+    private readonly string _tag = tag;
+
+    public void Log(object? message) => XarsuExports.Log($"[{_tag}] {message?.ToString() ?? "<null>"}");
+    public void LogWarning(object? message) => XarsuExports.LogWarning($"[{_tag}] {message?.ToString() ?? "<null>"}");
+    public void LogError(object? message) => XarsuExports.LogError($"[{_tag}] {message?.ToString() ?? "<null>"}");
+    public void LogVerbose(object? message) => XarsuExports.LogVerbose($"[{_tag}] {message?.ToString() ?? "<null>"}");
+}
